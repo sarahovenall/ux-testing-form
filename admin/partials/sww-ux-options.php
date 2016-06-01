abcd3b62d4ef3f6f977663b859cf25a6749f933c
Sarah Ovenall <sarah.ovenall@gmail.com>
2016-06-01 18:42:37 -0400

    Add files via upload

---
+++ admin/partials/sww-ux-options.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: saoven
+ * Date: 6/1/2016
+ * Time: 4:12 PM
+ */
+
+ ?>
+
+<h2>UX Testing Options</h2>
+
+<p>Enter the task list here.</p>
+
+<?php cmb2_metabox_form( $this->metabox_id, $this->key ); ?>
+
+<?php

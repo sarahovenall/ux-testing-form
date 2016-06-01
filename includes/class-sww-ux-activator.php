c768dcda6259ad753ff3dc493f6173ad6f715085
Sarah Ovenall <sarah.ovenall@gmail.com>
2016-06-01 18:43:42 -0400

    Add files via upload

---
+++ includes/class-sww-ux-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://inside.sas.com
+ * @since      1.0.0
+ *
+ * @package    Sww_Ux
+ * @subpackage Sww_Ux/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sww_Ux
+ * @subpackage Sww_Ux/includes
+ * @author     Sarah Ovenall, the SWW Team <sarah.ovenall@sas.com>
+ */
+class Sww_Ux_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-sww-ux-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://inside.sas.com
+ * @since      1.0.0
+ *
+ * @package    Sww_Ux
+ * @subpackage Sww_Ux/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sww_Ux
+ * @subpackage Sww_Ux/includes
+ * @author     Sarah Ovenall, the SWW Team <sarah.ovenall@sas.com>
+ */
+class Sww_Ux_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
